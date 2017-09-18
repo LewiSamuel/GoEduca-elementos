abee0364a1bc0ba6d168b7be42611be259e5bffb
Lewi Samuel <arroba.lewi@gmail.com>
2017-09-18 17:32:02 -0300

    geral

---
+++ nav.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<meta charset="utf-8">
+<head>
+	<title>NAV-TOP</title>
+	
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+
+    <body>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+   	
+
+
+
+  <nav>
+    <div class="row nav-wrapper white">
+      <a href="#" class="brand-logo grey-text text-darken-2">Go Educa</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class="grey-text text-darken-2" href="sass.html">Sass</a></li>
+        <li><a class="grey-text text-darken-2" href="badges.html">Components</a></li>
+        <li>
+          <a class="grey-text text-darken-2" href="collapsible.html">
+              <img class="circle" src="rsz_lewi.jpg" width="35px">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+        
+ 
+
+
+</body>
+</html>
